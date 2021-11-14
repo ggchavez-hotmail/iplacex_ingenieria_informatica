6b29b2e88e5a98cbe7344db7dd6af083e02df291
ggchavez-hotmail <69687643+ggchavez-hotmail@users.noreply.github.com>
2021-11-13 22:18:56 -0300

    examen final

---
+++ 2_patrones_de_diseño/examen_final/src/patroncomposite/SipenEmpleados.java
@@ -0,0 +1,14 @@
+package patroncomposite;
+
+public class SipenEmpleados extends CompositePension {
+    private int cantidadSipenEmpleados;
+
+    public int getCantidadSipenEmpleados() {
+        return cantidadSipenEmpleados;
+    }
+
+    public void setCantidadSipenEmpleados(int cantidadSipenEmpleados) {
+        this.cantidadSipenEmpleados = cantidadSipenEmpleados;
+    }
+
+}
---
+++ 2_patrones_de_diseño/examen_final/src/patroncomposite/SipenFFAA.java
@@ -0,0 +1,14 @@
+package patroncomposite;
+
+public class SipenFFAA extends CompositePension {
+    private int cantidadSipenFFAA;
+
+    public int getCantidadSipenFFAA() {
+        return cantidadSipenFFAA;
+    }
+
+    public void setCantidadSipenFFAA(int cantidadSipenFFAA) {
+        this.cantidadSipenFFAA = cantidadSipenFFAA;
+    }
+
+}
